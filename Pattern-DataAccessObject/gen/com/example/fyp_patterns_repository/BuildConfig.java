53687e6003fb4a6bc5a9961777390f954009c81c
Steve Ng <steveng.1988@gmail.com>
2013-11-02 06:34:20 +0800

    Added data access pattern example

---
+++ Pattern-DataAccessObject/gen/com/example/fyp_patterns_repository/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.fyp_patterns_repository;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
